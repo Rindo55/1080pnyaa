43c50fc5c1db7cb2f56cd9f77c8faea56c24ff2e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-20 17:15:07 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -26,7 +26,7 @@ def parse():
         item = {}
         item['title'] = trim_title(i['title'])
         item['size'] = i['nyaa_size']
-        item['link'] = mag + i['link']
+        item['link'] = mag + i['nyaa_infoHash']
         data.append(item)
     data.reverse()
     return data
