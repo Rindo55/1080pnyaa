be3c836b370a32d35159b6ad7dc1bab7443a037d
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-20 20:02:37 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -9,8 +9,7 @@ from main import queue
 from main.inline import button1
 
 def trim_title(title: str):
-    title, ext = title.replace("[Magnet]",""), ext = ".mkv"
-    title = title.strip() + ext
+    title = title.replace("[Magnet]","")
     title = title.replace("Ijiranaide, Nagatoro-san S2", "Ijiranaide, Nagatoro-san 2")
     title = title.replace("Shinka", "Shin Shinka")
     return title
